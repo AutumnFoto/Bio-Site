2cc20731dc4364e48c9c426077bd641f53c2ffb4
Autumn Fotopoulos <acarte94@gmail.com>
2020-10-05 16:53:16 -0500

    styling and adding projects

---
+++ src/javascripts/data/projectData.js
@@ -0,0 +1,51 @@
+const projects = [
+  {
+    title: 'Spill the Jeans',
+    screenshot: '',
+    description: 'enter here',
+    technologiesUsed: 'HTML, CSS, JavaScript, Version Control with Github',
+    available: true,
+    url: 'enter here',
+    githubUrl: 'enter here'
+  },
+  {
+    title: 'Pet Adoption Site',
+    screenshot: '',
+    description: 'enter here',
+    technologiesUsed: 'HTML, CSS, JavaScript, Version Control with Github',
+    available: true,
+    url: 'enter here',
+    githubUrl: 'enter here'
+  },
+  {
+    title: 'Cool Project',
+    screenshot: '',
+    description: 'This is the best project',
+    technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
+    available: false,
+    url: 'enter here',
+    githubUrl: 'enter here'
+  },
+  {
+    title: 'Another Cool Project',
+    screenshot: '',
+    description: 'enter here',
+    technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
+    available: false,
+    url: 'enter here',
+    githubUrl: 'enter here'
+  },
+  {
+    title: 'Another Cool Project',
+    screenshot: '',
+    description: 'enter here',
+    technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
+    available: false,
+    url: 'enter here',
+    githubUrl: 'enter here'
+  }
+];
+
+const getProjectData = () => projects;
+
+export default { getProjectData };
